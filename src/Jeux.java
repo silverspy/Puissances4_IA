7d6dcdc4c2814736e845e6e2b853fc8113c822b7
Gilles Labrousse <gilles.labrousse@etu.univ-tlse2.fr>
2019-02-04 17:49:48 +0100

    1st try groupejetons

--- src/Jeux.java
+++ src/Jeux.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Jeux {
     String[][] plateaux = new String[6][7];
+    HashSet<ArrayList<int[]>> plusGrosGroupe = new HashSet();
 
     public Jeux(String[][] plateaux) {
         this.plateaux = plateaux;
@@ -10,17 +15,54 @@ public class Jeux {
         return plateaux;
     }
 
-    public boolean gagnerpartie() {
+    public void nbjetons() {
         for (String[] c : plateaux) {
             for (int i = 0; i < plateaux.length; i++) {
                 for (int j = 0; j < plateaux[i].length; j++) {
                     if (plateaux[i][j] != " ") {
+                        String joueur = plateaux[i][j];
+                        for (int k = -4; k < 4; k++) {
+                            ArrayList<int[]> paquetcol = new ArrayList<>(); // Col
+                            if (plateaux[i + k][j] == joueur && plateaux.length < i+k) { //Ligne
+                                paquetcol.add(new int[]{i, j});
+                            } else {
+                                if (!paquetcol.isEmpty()) {
+                                    plusGrosGroupe.add(paquetcol);
+                                }
+                            }
 
+                            ArrayList<int[]> paquetLig = new ArrayList<>(); // lig
+                            if (plateaux[i][j + k] == joueur && plateaux[i].length < j+k) { //Ligne
+                                paquetLig.add(new int[]{i, j});
+                            } else {
+                                if (!paquetLig.isEmpty()) {
+                                    plusGrosGroupe.add(paquetLig);
+                                }
+                            }
+
+                            ArrayList<int[]> paquetDiag1 = new ArrayList<>(); // diag1
+                            if (plateaux[i + k][j + k] == joueur && plateaux[i].length < j+k && plateaux.length < i+k) { //Ligne
+                                paquetDiag1.add(new int[]{i, j});
+                            } else {
+                                if (!paquetDiag1.isEmpty()) {
+                                    plusGrosGroupe.add(paquetDiag1);
+                                }
+                            }
+
+                            ArrayList<int[]> paquetDiag2 = new ArrayList<>(); // diag2
+                            if (plateaux[i + k][j + k * - 1] == joueur && plateaux[i].length < j+k && plateaux.length < i+k) { //Ligne
+                                paquetDiag2.add(new int[]{i, j});
+                            } else {
+                                if (!paquetDiag2.isEmpty()) {
+                                    plusGrosGroupe.add(paquetDiag2);
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-        return false;
+        System.out.println(plusGrosGroupe);
     }
 }
-    
+
